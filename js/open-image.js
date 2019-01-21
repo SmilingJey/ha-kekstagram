0fec570d16716b9161c796bd558f97acf4acf8d6
Evsyakov Artem <evsyakov@gmail.com>
2019-01-21 22:45:18 +0300

    добавлен выбор эффекта

--- js/open-image.js
+++ js/open-image.js
@@ -1,28 +1,47 @@
 (function(){
-  var FILE_TYPES = ['gif', 'jpg', 'jpeg', 'png'];
-
   var uploadFormElement = document.querySelector('#upload-select-image');
   var inputUploadFileElement = uploadFormElement.querySelector('#upload-file');
   var imgPreviewElement = uploadFormElement.querySelector('.img-upload__preview img');
   var filterPreviewElements = uploadFormElement.querySelectorAll('.effects__preview');
   var uploadEditorElement = uploadFormElement.querySelector('.img-upload__overlay');
   var closeButtonElement = uploadFormElement.querySelector('.img-upload__cancel');
+  var effectLevelElement = uploadFormElement.querySelector('.img-upload__effect-level');
+  var effectLevelValueElement = uploadFormElement.querySelector('.effect-level__value');
+  var effectsListElement = uploadFormElement.querySelector('.effects__list');
+  var scaleSmallerElement = uploadFormElement.querySelector('.scale__control--smaller');
+  var scaleBiggerElement = uploadFormElement.querySelector('.scale__control--bigger');
+  var scaleValueElement = uploadFormElement.querySelector('.scale__control--value');
 
-  var onUploadImage = function(evt) {
-    showUploadEditior(evt.target.result);
-  }
-
-  inputUploadFileElement.addEventListener('change', function(){
-    window.loadInputFiles(inputUploadFileElement.files, onUploadImage, FILE_TYPES);
-  });
-
+  //закрытие формы
   closeButtonElement.addEventListener('click', function () {
-    uploadFormElement.reset();
     closeUploadEditior();
   });
 
   var onUploadEditorEsc = function(evt) {
     window.util.isEscEvent(evt, closeUploadEditior);
+  }
+
+  var closeUploadEditior = function(img) {
+    uploadFormElement.reset();
+    uploadEditorElement.classList.add('hidden');
+    document.removeEventListener('keydown', onUploadEditorEsc);
+  }
+
+  // загрузка изображения
+  var onUploadImage = function(evt) {
+    showUploadEditior(evt.target.result);
+  }
+
+  inputUploadFileElement.addEventListener('change', function() {
+    var FILE_TYPES = ['gif', 'jpg', 'jpeg', 'png'];
+    window.loadInputFiles(inputUploadFileElement.files, onUploadImage, FILE_TYPES);
+  });
+
+  //открытие формы
+  var resetEditor = function() {
+    setScale(100);
+    effectLevelElement.classList.add('hidden');
+    setEffect();
   }
 
   var showUploadEditior = function(img) {
@@ -32,12 +51,44 @@
     });
     uploadEditorElement.classList.remove('hidden');
     document.addEventListener('keydown', onUploadEditorEsc);
+    resetEditor();
   }
 
-  var closeUploadEditior = function(img) {
-    uploadEditorElement.classList.add('hidden');
-    document.removeEventListener('keydown', onUploadEditorEsc);
+  //изменение выбранного эффекты
+  var onChangeEffect = function(evt) {
+    if (evt.target.value === 'none') effectLevelElement.classList.add('hidden');
+    else effectLevelElement.classList.remove('hidden');
+    effectLevelValueElement.value = 100;
+    setEffect();
   }
 
+  effectsListElement.addEventListener('change', onChangeEffect);
 
+  var setEffect = function() {
+    var effect = uploadFormElement.querySelector('.effects__radio:checked').value;
+    var value = effectLevelValueElement.value;
+    var filter = "none";
+    if (effect === 'chrome') filter = `grayscale(${value})`;
+    else if (effect === 'sepia') filter = `sepia(${value})`;
+    else if (effect === 'marvin') filter = `invert(${value*100}%)`;
+    else if (effect === 'phobos') filter = `blur(${value*3}px)`;
+    else if (effect === 'heat') filter = `brightness(${value*2 + 1})`;
+    imgPreviewElement.style.filter = filter;
+  }
+
+  //изменение масштаба
+  var setScale = function(scale){
+    if (scale > 100) scale = 100;
+    if (scale < 25) scale = 25;
+    scaleValueElement.value = scale + '%';
+    imgPreviewElement.style.transform = `scale(${scale/100})`;
+  };
+
+  scaleSmallerElement.addEventListener('click', function() {
+    setScale(parseInt(scaleValueElement.value) - 25);
+  });
+
+  scaleBiggerElement.addEventListener('click', function() {
+    setScale(parseInt(scaleValueElement.value) + 25);
+  });
 })();
